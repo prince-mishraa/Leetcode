f90512775d2d195defbc4f5cdbfb73606af39d7f
Prince Mishra <princemishra8104@gmail.com>
2025-08-02 00:11:15 +0530

    Time: 8 ms (26.98%) | Memory: 23.9 MB (31.42%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,9 +4,9 @@ public:
         int n = intervals.size();
 
         sort(intervals.begin(),intervals.end());
-        
+
         vector<vector<int>> ans;
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i < n;i++){
             if(ans.empty() || intervals[i][0] > ans.back()[1]){
                 ans.push_back(intervals[i]);
             }
@@ -14,6 +14,6 @@ public:
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
-    return ans;
+        return ans;
     }
 };
