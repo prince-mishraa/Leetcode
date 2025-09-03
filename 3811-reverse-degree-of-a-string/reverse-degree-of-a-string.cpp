e2ea570c354e1a9b9da4a99e88cda52ac9e8e157
Prince Mishra <princemishra8104@gmail.com>
2025-09-03 22:40:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.15%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+        for(int i = 0; i < s.length();i++){
+            int num = 26 -(s[i] - 'a');
+            ans+= num * (i+1);
+        }
+        return ans;
+    }
+};
