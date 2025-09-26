ffeacd5c0f6638ab1cbe89c65f2880e03014ae0e
Prince Mishra <princemishra8104@gmail.com>
2025-09-26 23:11:50 +0530

    Time: 3 ms (69.85%) | Memory: 14.8 MB (36.04%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         map <int, int> mpp;
 
-        for(int i = 0;i < n;i++){
+        for(int i = 0; i < n;i++){
             int a = nums[i];
             int more = target - a;
             if(mpp.find(more) != mpp.end()){
