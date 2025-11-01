183878cbfd137dc3bb897b1065e2ca60275f76b7
Prince Mishra <princemishra8104@gmail.com>
2025-11-01 23:17:30 +0530

    Time: 1 ms (52.55%) | Memory: 11 MB (38.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        s+= " ";
+        s += " ";
         stack<string> st;
         string str = "";
-        
-        for(int i = 0; i< s.length(); i++){
+
+        for (int i = 0; i < s.length(); i++) {
             if (s[i] == ' ') {
                 if (!str.empty()) {
                     st.push(str);
@@ -19,7 +19,8 @@ public:
         while (!st.empty()) {
             ans += st.top();
             st.pop();
-            if (!st.empty()) ans += " ";
+            if (!st.empty())
+                ans += " ";
         }
         return ans;
     }
