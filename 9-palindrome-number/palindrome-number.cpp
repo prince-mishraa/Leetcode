1f5e7e7b6a137b035344d39cc2f8cc83e001f2db
Prince Mishra <princemishra8104@gmail.com>
2026-02-24 11:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0 || (x%10 == 0 && x!= 0)) return false;
+        if(x < 0 || (x % 10 == 0 && x != 0))
+            return false;
 
         int revnum = 0;
-        int og = x;
+        int original = 0;
         while(x > revnum){
-            revnum = (revnum * 10) + x%10;
-            x = x/10;
+            revnum = (revnum * 10) + x % 10;
+            x = x / 10; 
         }
-        return (revnum == x || x == revnum/10);
+        return (revnum == x || x == revnum / 10);
     }
 };
